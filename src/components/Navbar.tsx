dfc8313786d2fbb1e0ea87fea8badbcd3b8c4ed8
Daehwa Seo <sdhwa92@gmail.com>
2023-07-18 22:25:49 +1000

    implement OAuth sign in by using next-auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import { Icons } from "./Icons";
-import { buttonVariants } from "./ui/Button";
+import { Button, buttonVariants } from "./ui/Button";
+import { getAuthSession } from "@/lib/auth";
+import { signOut } from "next-auth/react";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getAuthSession();
+
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
@@ -16,9 +20,13 @@ const Navbar = () => {
 
         {/* search bar */}
 
-        <Link href="/sign-in" className={buttonVariants()}>
-          Sign In
-        </Link>
+        {session ? (
+          <Button className={buttonVariants()}>Sign Out</Button>
+        ) : (
+          <Link href="/sign-in" className={buttonVariants()}>
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );
